2a3a8764a546b9d6463d3039c3fa8e014334421b
lysenko_v <vlad.lysenko1999@gmail.com>
2022-10-02 10:09:44 +0300

    Add class LinearEquations with function Equation_1, Equation_2.

---
+++ LinearEquations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpForUniversity
+{
+    internal class LinearEquations
+    {
+        public static void Equation_1(decimal value)
+        {
+
+        }
+        public static void Equation_2(decimal value)
+        {
+
+        }
+    }
+}
