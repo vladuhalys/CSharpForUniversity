4da7e735fa25e7b590c4f1850b145a75f6f57894
vladuhalys <vlad.lysenko1999@gmail.com>
2022-10-06 12:50:05 +0300

    Ex 1 - Done.

--- LinearEquations.cs
+++ LinearEquations.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static System.Math;
 
 namespace CSharpForUniversity
 {
     internal class LinearEquations
     {
-        public static void Equation_1(decimal value)
+        public static double Equation_1(double A)
         {
-
+            //z1 = cosA + cos2A + cos6A + cos7A
+            double z1 = Cos(A) + Cos(2*A) + Cos(6*A) + Cos(7 * A);
+            return z1;
         }
-        public static void Equation_2(decimal value)
+        public static double Equation_2(double A)
         {
-
+            //z2 = 4 cos A/2 * cos 5/2 * cos4A
+            double z2 = 4 * Cos(A / 2) * Cos(5 / 2) * Cos(4 * A);
+            return z2;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
-﻿using System.Net.Http.Headers;
+﻿using static System.Console;
 
 namespace CSharpForUniversity
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           
+            bool flag, trigger;
+            char sign;
+            do
+            {
+                Clear();
+                flag = false;
+                try
+                {
+                    Write("A->");
+                    double A = Convert.ToDouble(ReadLine());
+                    WriteLine($"z1 = cosA + cos2A + cos6A + cos7A\nz1 = {LinearEquations.Equation_1(A)}\n\n");
+                    WriteLine($"z2 = 4 cos A/2 * cos 5/2 * cos4A\nz2 = {LinearEquations.Equation_2(A)}\n\n");
+                }
+                catch (Exception ex)
+                {
+                    WriteLine(ex.Message);
+                }
+                finally
+                {
+                    do
+                    {
+                        trigger = false;
+                        Write("Continue? [Y/N] => ");
+                        sign = ReadKey().KeyChar;
+                        if (char.ToUpper(sign) == 'N' || char.ToUpper(sign) == 'Y')
+                        {
+                            trigger = true;
+                            if(char.ToUpper(sign) == 'N')
+                            {
+                                flag = true;
+                            }
+                        }          
+                    } while (trigger != true);
+                    
+                }
+            } while (flag != true);
+            
+            
         }
     }                             
 }
