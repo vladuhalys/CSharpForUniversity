c0fa2c87223149e2cee4434b54417b38a2a709f0
vladuhalys <vlad.lysenko1999@gmail.com>
2022-10-11 11:23:22 +0300

    Add class Vector3D

---
+++ Vector3D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpForUniversity
+{
+    public class Vector3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Vector3D()
+        {
+            X = Y = Z = 0;
+        }
+        public Vector3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public override string ToString()
+        {
+            return $"({X}; {Z}; {Y})";
+        }
+    }
+}
